c95f3aecc0c0fd56060f2d22be0d43186e25d82f
Sandy Akbar <55833676+sandytwelve12@users.noreply.github.com>
2019-09-26 05:42:33 -0700

    Add files via upload

---
+++ PBO-ULANG-10114504-Latihan12-Aritmatika/src/pbo/ulang/pkg10114504/latihan12/aritmatika/PBOULANG10114504Latihan12Aritmatika.java
@@ -0,0 +1,42 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package pbo.ulang.pkg10114504.latihan12.aritmatika;
+
+/**
+ *
+ * @author 
+ * NAMA     : Sandy Akbar
+ * KELAS    : PBO-Ulang
+ * NIM      : 10114504
+ * DESKRIPSI: Program ini berisi program Aritmatika
+ */
+public class PBOULANG10114504Latihan12Aritmatika {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        int a = 10;
+        int b = 20;
+        int c = 25;
+        int d = 25;
+        System.out.println("a + b = " + (a + b ));
+        System.out.println("a - b = " + (a - b ));
+        System.out.println("a * b = " + (a * b ));
+        System.out.println("b / a = " + (b / a ));
+        System.out.println("b % a = " + (b % a ));
+        System.out.println("b % a = " + (c % a ));
+        System.out.println("a++ = " + (a++ ));
+        System.out.println("b-- = " + (a-- ));
+        System.out.println("d++ = " + (d++ ));
+        System.out.println("++d = " + (++d ));
+        
+                
+        
+    }
+    
+}
